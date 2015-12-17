3974090e6550ecc6b61a230119a35864c26b1649
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2015-12-17 11:30:21 +0000

    fixed CustomerDataResponse test

--- src/test/groovy/com/buyexpressly/api/resource/merchant/CustomerDataResponseSpec.groovy
+++ src/test/groovy/com/buyexpressly/api/resource/merchant/CustomerDataResponseSpec.groovy
@@ -1,48 +1,21 @@
 package com.buyexpressly.api.resource.merchant
 
+import com.buyexpressly.api.resource.server.Address
+import com.buyexpressly.api.resource.server.Customer
+import com.buyexpressly.api.resource.server.CustomerData
+import com.buyexpressly.api.resource.server.Email
+import com.buyexpressly.api.resource.server.Metadata
+import com.buyexpressly.api.resource.server.Phone
+import com.buyexpressly.api.resource.server.Tuple
+import org.codehaus.jackson.map.ObjectMapper
+import org.joda.time.DateTime
+import org.joda.time.LocalDate
 import spock.lang.Specification
 
 class CustomerDataResponseSpec extends Specification {
     def "a customer can be mapped from a json string"() {
-        when:
-        true
-        then:
-        true
-    }
-/*        given: "I have a customer in its json representation"
-
-        String json =
-                '{' +
-
-                '"metadata" :  {' +
-                        '                  "sender" : "http://www.shop.com",' +
-                        '                  "locale" : "GBR",' +
-                        '                  "issuerData" : [' +
-                        '                      {' +
-                        '                        "field" : "lang",' +
-                        '                        "value" : "eng"' +
-                        '                      }' +
-                        '                    ]' +
-                        '                },'
-                '"data" : {' +
-                '"customerData" : {' +
-                '"firstName":"Marc",' +
-                '"lastName":"Smith",' +
-                '"gender":"M",' +
-                '"billingAddress":0,' +
-                '"shippingAddress":0,' +
-                '"dateUpdated":"2015-11-25T13:21:04.000Z",' +
-                '"numberOrdered":0,' +
-                '"onlinePresence":[{"field":"twitter","value":"mgsmith57"}],' +
-                '"emails":[{"email":"someone@test.com"}],' +
-                '"phones":[{"type":"L","number":"555 123","countryCode":44}],' +
-                '"addresses":[{"firstName":"Marc","lastName":"SMith","address1":"261 Wellfield Road","city":"Roath","zip":"CF24 3DG","phone":0,"stateProvince":"Cardiff","country":"GB"}]' +
-                        '}'+
-                        '"email" : "a@email.com"' +
-
-                        '}'+
-                        '}'
-
+        given: "I have a customer data response in its json representation"
+        String json = generateCustomerDataResponseString()
 
         when: "I map it"
         CustomerDataResponse entity = new ObjectMapper().readValue(json, CustomerDataResponse)
@@ -78,29 +51,41 @@ class CustomerDataResponseSpec extends Specification {
 
         when: "I use the builder"
         CustomerDataResponse entity = CustomerDataResponse.builder()
-            .withBillingAddress(1)
-            .withCompany("expressly")
-            .withDob(LocalDate.parse('1979-03-12'))
-            .withFirstName('Marc')
-            .withGender('M')
-            .withLastName('Smith')
-            .withLastOrderTimeAtSource(LocalDate.parse('2015-10-19'))
-            .withLastUpdatedAtSource(DateTime.parse('2015-11-25T13:21:04+00:00'))
-            .withOrderItemCount(10)
-            .withShippingAddress(2)
-            .withTaxNumber("tax")
-            .addAddress(mapper.readValue('{"address1":"addr1"}', Address))
-            .addAddress(mapper.readValue('{"address1":"addr2"}', Address))
-            .addEmailAddress(mapper.readValue('{"email":"one@test.com"}', Email))
-            .addEmailAddress(mapper.readValue('{"email":"two@test.com"}', Email))
-            .addOnlineIdentity(mapper.readValue('{"field":"twitter","value":"expressly"}', Tuple))
-            .addOnlineIdentity(mapper.readValue('{"field":"facebook","value":"expressly"}', Tuple))
-            .addPhoneNumber(mapper.readValue('{"type":"L","number":"555 123"}', Phone))
-            .addPhoneNumber(mapper.readValue('{"type":"M","number":"555 678"}', Phone))
-            .build()
+                .withData(Customer.build(CustomerData.builder()
+                .withBillingAddress(1)
+                .withCompany("expressly")
+                .withDob(LocalDate.parse('1979-03-12'))
+                .withFirstName('Marc')
+                .withGender('M')
+                .withLastName('Smith')
+                .withLastOrderTimeAtSource(LocalDate.parse('2015-10-19'))
+                .withLastUpdatedAtSource(DateTime.parse('2015-11-25T13:21:04+00:00'))
+                .withOrderItemCount(10)
+                .withShippingAddress(2)
+                .withTaxNumber("tax")
+                .addAddress(mapper.readValue('{"address1":"addr1"}', Address))
+                .addAddress(mapper.readValue('{"address1":"addr2"}', Address))
+                .addEmailAddress(mapper.readValue('{"email":"one@test.com"}', Email))
+                .addEmailAddress(mapper.readValue('{"email":"two@test.com"}', Email))
+                .addOnlineIdentity(mapper.readValue('{"field":"twitter","value":"expressly"}', Tuple))
+                .addOnlineIdentity(mapper.readValue('{"field":"facebook","value":"expressly"}', Tuple))
+                .addPhoneNumber(mapper.readValue('{"type":"L","number":"555 123"}', Phone))
+                .addPhoneNumber(mapper.readValue('{"type":"M","number":"555 678"}', Phone))
+                .build(),
+                "a@email.com"))
+                .withMeta(
+                Metadata.build("http://www.shop.com", "GBR", Tuple.build("lang", "eng")))
+                .build()
 
         then: "I see that the values are set correctly"
-        with(entity) {
+        entity.data.email == "a@email.com"
+        with(entity.meta) {
+            sender == "http://www.shop.com"
+            locale == "GBR"
+            issuerData.get(0).field == "lang"
+            issuerData.get(0).value == "eng"
+        }
+        with(entity.data.customerData) {
             firstName == 'Marc'
             lastName == 'Smith'
             gender == 'M'
@@ -122,28 +107,63 @@ class CustomerDataResponseSpec extends Specification {
             addresses[1].address1 == 'addr2'
         }
     }
-    def "I can build a customer with only a first, last name, gender and one address "() {
-        given: "I have an object mapper to create the entities that don't have builders yet"
-        ObjectMapper mapper = new ObjectMapper()
 
-        when: "I use the builder"
-        CustomerDataResponse entity = CustomerDataResponse.builder()
-                .withFirstName('Marc')
-                .withGender('M')
-                .withLastName('Smith')
-                .addAddress(mapper.readValue('{"address1":"addr1"}', Address))
-                .build()
-
-        then: "I see that the values are set correctly"
-        with(entity) {
-            firstName == 'Marc'
-            lastName == 'Smith'
-            gender == 'M'
-            billingAddress == 0
-            shippingAddress == 0
-            addresses[0].address1 == 'addr1'
+    def String generateCustomerDataResponseString() {
+        return """{
+  "meta": {
+    "sender": "http://www.shop.com",
+    "locale": "GBR",
+    "issuerData": [
+      {
+        "field": "lang",
+        "value": "eng"
+      }
+    ]
+  },
+  "data": {
+    "customerData": {
+      "firstName": "Marc",
+      "lastName": "Smith",
+      "gender": "M",
+      "billingAddress": 0,
+      "shippingAddress": 0,
+      "dateUpdated": "2015-11-25T13:21:04.000Z",
+      "numberOrdered": 0,
+      "onlinePresence": [
+        {
+          "field": "twitter",
+          "value": "mgsmith57"
         }
+      ],
+      "emails": [
+        {
+          "email": "someone@test.com"
+        }
+      ],
+      "phones": [
+        {
+          "type": "L",
+          "number": "555 123",
+          "countryCode": 44
+        }
+      ],
+      "addresses": [
+        {
+          "firstName": "Marc",
+          "lastName": "SMith",
+          "address1": "261 Wellfield Road",
+          "city": "Roath",
+          "zip": "CF24 3DG",
+          "phone": 0,
+          "stateProvince": "Cardiff",
+          "country": "GB"
+        }
+      ]
+    },
+    "email": "a@email.com"
+  }
+}
+"""
     }
-*/
 
 }
